c02cb6be253265da7652702add23bd24a1c98789
labas02 <zdenda.cejka112@seznam.cz>
2024-08-29 15:51:58 +0200

    push

---
+++ clone.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+cp ../.config/bspwm/bspwmrc .
+cp ../.config/sxhkd/sxhkdrc .
+cp ../.config/polybar/config.ini .
+cp ../.config/picom/picom.sample.conf .
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+mkdir ../.config ../.config/bspwm ../.config/sxhkd 
+mkdir ../.config/picom ../.config/polybar ../.config/dunst
+cp bspwmrc ../.config/bspwm/
+cp sxhkdrc ../.config/sxhkd/
+cp config.ini ../.config/polybar/
