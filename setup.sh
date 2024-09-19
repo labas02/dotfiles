66f19f21704b9b826aec29d960a06b0a85ee162b
zdenek cejka <96704507+labas02@users.noreply.github.com>
2024-09-19 12:27:59 +0200

    xinitrc update

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ cp bspwmrc ../.config/bspwm/
 cp sxhkdrc ../.config/sxhkd/
 cp config.ini ../.config/polybar/
 cp picom.sample.conf ../.config/picom/
-cp xinitrc ../
+cp .xinitrc ../
 cp alacritty.toml ../.config/alacritty
